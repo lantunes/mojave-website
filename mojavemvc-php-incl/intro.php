19028dce3077121204d11dcffb58a088ed776771
Luis Antunes <lantunes@gmail.com>
2012-12-22 12:25:55 -0500

    adding controller documentation

--- mojavemvc-php-incl/intro.php
+++ mojavemvc-php-incl/intro.php
@@ -1,20 +1,20 @@
 <table width="500px" border="0" cellpadding="10" cellspacing="10">
  <tr>
   <td>
-   <p style="font-family: arial;font-size:10pt">
+   <p class="regtext">
 Mojave MVC is an annotation-driven, POJO-based web application development framework for Java. 
 It borrows ideas from Spring Web MVC and EJB 3.1, and incorporates Guice. It attempts to:
    </p>
-   <ul style="padding-left:20px;font-family: arial;font-size:10pt">
+   <ul class="list">
      <li class="listitem">facilitate TDD and Dependency Injection, and ultimately the development of decoupled components, by providing IoC capabilities</li>
      <li class="listitem">remove concurrency concerns from web application development by providing a single-thread programming model</li>
      <li class="listitem">remove cross-cutting concerns from web application development by supporting AOP through the interceptor pattern</li>
      <li class="listitem">be as minimally intrusive a framework as possible; it tries to get out of your way by minimizing framework-related metadata and biolerplate code</li>
    </ul>
-   <p style="font-family: arial;font-size:10pt">
+   <p class="regtext">
 Mojave also:
    </p>
-   <ul style="padding-left:20px;font-family: arial;font-size:10pt">
+   <ul class="list">
      <li class="listitem">supports building RESTful applications: controller actions can be bound to specific HTTP methods and URIs</li>
      <li class="listitem">supports JSP out-of-the-box</li>		
      <li class="listitem">is small and lightweight</li>
--- mojavemvc/controllers.php
+++ mojavemvc/controllers.php
@@ -19,5 +19,104 @@ here. To learn more about actions, please see the
 <a class="sitelink" href="actions">actions</a> documentation page.
 </p>
 
+<h2>Controller Types</h2>
+
+<p class="regtext">
+There are three types of controllers in the Mojave framework:
+</p>
+
+<ul class="list">
+ <li class="listitem">Stateless</li>
+ <li class="listitem">Stateful</li>
+ <li class="listitem">Singleton</li>
+</ul>
+
+<h2>Stateless Controller</h2>
+
+<p class="regtext">
+A new instance of this controller is created per request. Its dependencies are 
+injected after creation.
+</p>
+
+<p class="regtext">
+The following are valid declarations of stateless controllers:
+</p>
+
+<?php 
+echo geshify('
+@StatelessController
+public class SomeController {
+ ...
+}
+ 
+@StatelessController("some-name")
+public class SomeController {
+ ...
+}', 'java5');
+?>
+ 
+<p class="regtext">
+It is important to note that as a new instance of a stateless controller is created with every 
+request, the developer should not design the stateless controller with state. That is, class 
+fields should not be used with the intention of storing state between requests, but they can be 
+used for storing state in the object during a request. For storing state between requests, 
+use a StatefulController.
+</p>
+
+<h2>Stateful Controller</h2>
+
+<p class="regtext">
+Only one instance of this controller will exist per request session. It is kept in the HttpSession. 
+The instance goes away when the session is invalidated, the session times out, or the application 
+is restarted. Its dependencies, if any, are re-injected with every new request.
+</p>
+
+<p class="regtext">
+The following are valid declarations of stateful controllers:
+</p>
+
+<?php 
+echo geshify('
+@StatefulController
+public class SomeController {
+ ...
+}
+ 
+@StatefulController("some-name")
+public class SomeController {
+ ...
+}', 'java5');
+?>
+
+<h2>Singleton Controller</h2>
+
+<p class="regtext">
+Only one instance of this controller will exist in the application, placed in the front controller 
+context. Its dependencies, if any, are re-injected with every new request.
+</p>
+
+<p class="regtext">
+The following are valid declarations of singleton controllers:
+</p>
+
+<?php 
+echo geshify('
+@SingletonController
+public class SomeController {
+ ...
+}
+ 
+@SingletonController("some-name")
+public class SomeController {
+ ...
+}', 'java5');
+?>
+
+<p class="regtext">
+Note that it is up to the developer to address thread-safety in singleton controllers. The single 
+instance will be accessed simultaneously by multiple threads, and thus there will be concurrency 
+issues to address. The container will not attempt to address those issues.
+</p>
+
 <?php include "../mojavemvc-php-incl/docs-bottom.php" ?>
 <?php include "../mojavemvc-php-incl/page-bottom.php" ?>
