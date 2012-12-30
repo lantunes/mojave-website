8921d6bc5ea902f1c29652f2fe971e53157d106c
Luis Antunes <lantunes@gmail.com>
2012-12-30 07:13:21 -0500

    updating FAQ

--- mojavemvc/faq.php
+++ mojavemvc/faq.php
@@ -12,12 +12,12 @@ introduction of yet another web framework for Java. Mojave was created
 to meet several needs. The first was the need for a framework that 
 leads to code free of boilerplate that is very easy to test. The second 
 was the need for a framework supporting full Dependency Injection with
-an alternative DI container. Spring is a great framework, but if you
-didn't want to use the Spring DI container, there were not many choices
-of frameworks supporting both an alternative DI container, such as Guice,
+an alternative IoC container. Spring is a great framework, but if you
+didn't want to use the Spring IoC container, there were not many choices
+of frameworks supporting both an alternative IoC container, such as Guice,
 and the MVC paradigm. Finally, Mojave attempts to be flexible in terms
 of the scenarios in which it can be used. For instance, it can be used
-to render JSPs, or as part of a RESTful service, where the content produced
+to render JSPs, or to provide a RESTful service, where the content produced
 is not necessarily meant to be rendered in a browser.
 </p>
 
