d5570fb5acd0b9056333846d546786e261c6777a
Luis Antunes <Lantunes@gmail.com>
2013-04-23 20:08:56 -0400

    fixing php include error

--- mojavemvc/downloads.php
+++ mojavemvc/downloads.php
@@ -1,6 +1,6 @@
 <?php define("TITLE", "Downloads"); ?>
 <?php include "../mojavemvc-php-incl/page-top.php" ?>
-<?php include_once 'geshify.php'; ?>
+<?php include_once "../mojavemvc-php-incl/geshify.php"; ?>
 
 <table width="400px" border="0" cellpadding="10" cellspacing="10">
  <tr>
@@ -10,13 +10,13 @@
     <tr>
 	  <td>
 	    <span class="smalltext">Apache Maven</span>
-	    <?php
-          echo geshify('<dependency>
-            <groupId>org.mojavemvc</groupId>
-            <artifactId>mojave-core</artifactId>
-            <version>1.0.6</version>
-          </dependency>','xml');
-        ?>
+<?php
+echo geshify('<dependency>
+  <groupId>org.mojavemvc</groupId>
+  <artifactId>mojave-core</artifactId>
+  <version>1.0.6</version>
+</dependency>','xml');
+?>
 	  </td>
 	</tr>
     <tr>
